073f4ea8d8475eb194c675ad2a2673aeedaed708
FilinMen <fiilinkadabra@tutamail.com>
2025-01-13 16:59:55 +0300

    ф

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,69 +1,71 @@
-
-import pandas as pd
-import numpy as np
+# отображение точек из натурального ряда
 import matplotlib.pyplot as plt
-import plotly as pl
-import plotly.express as px
 
 list_simpl_num =[1,2,3,5,7,11,13,17,19,23,29]
 list = []
 sov_num = [6,28]
-number = int(input("Please Enter any Number: "))  #
-for i in range(1, number + 1):                    #
-    list.append(int(i))                           #натуральный ряд
-print(list)
-
-
 x = list
 y = []
-a = 1
+x1 = []
+x2 = []
+x3 = []
+y1 = []
+y2 = []
+y3 = []
 
-for m in list:
-    for l in list:
+
+def listx(): #перебор элементов из списка y
+    for i in y:
+        return i
+
+def sumdel(m): #подсчет суммы делителей
+    a = 0
+    for l in range(1,max(list)):
         b = m%l
         if b == 0:
-            b = m/l
-            a += b
-    y.append(a) 
-    
-list_colorx = []
+            a += l
+        elif m in list_simpl_num:
+            a = 1 
+    y.append(int(a)) 
 
-for q in x:   
-    q = int(q)
-    if q in list_simpl_num:
-        i = 'b'
-        list_colorx.append(q)
-        list_colorx.append(i)
-    elif q in sov_num:
-        i = 'r'
-        list_colorx.append(q)
-        list_colorx.append(i)
-    else:
-        i = 'g'
-        list_colorx.append(q)
-        list_colorx.append(i)
-print(list_colorx)
+def Naturalrow(x): #натуральный ряд
+    for i in range(1, x + 1):                    
+        list.append(int(i))                           
 
-list_colory = []
-for q in y:   
-    q = int(q)
-    if q in list_simpl_num:
-        i = 'b'
-        list_colory.append(q)
-        list_colory.append(i)
-    elif q in sov_num:
-        i = 'r'
-        list_colory.append(q)
-        list_colory.append(i)
-    else:
-        i = 'g'
-        list_colory.append(q)
-        list_colory.append(i)
-print(list_colory)
+def color(): # распределение цветов 
+    for q in x: 
+        i = listx()  
+        q = int(q)
+        i = int(i)
+        if q in list_simpl_num:#green
+            y1.append(i)
+            x1.append(q)
+        elif q in sov_num:#red
+            y2.append(i)
+            x2.append(q)
+        else:#blue
+            y3.append(i)
+            x3.append(q)
+        y.remove(i)
 
-plt.scatter(x = list_colorx, y = list_colory)
-plt.legend(loc = 'upper right')
-plt.xlabel('Sum of divisors of number') #Подпись для оси х
-plt.ylabel('Sum of the divisors of a number') #Подпись для оси y
-plt.title('Types of numbers') #Название
-plt.show()
+def plo(): # отрисовка графика
+    plt.scatter(x1, y1, color = 'g', label = 'simpl_num')
+    plt.scatter(x2, y2, color = 'r', label = 'sov_num')
+    plt.scatter(x3, y3, color = 'b', label = 'another')
+    plt.legend(title = 'types of number')
+    plt.xlabel('Sum of divisors of number') #Подпись для оси х
+    plt.ylabel('Sum of the divisors of a number') #Подпись для оси y
+    plt.title('Types of numbers') #Название
+    plt.grid()
+    plt.show()
+
+def main(): 
+    number = int(input("Please Enter any Number: "))
+    Naturalrow(number)
+    for m in list:
+        sumdel(m)
+    color()
+    plo()
+
+if __name__ == "__main__":
+    main()
